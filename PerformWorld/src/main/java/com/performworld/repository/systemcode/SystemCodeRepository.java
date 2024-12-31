577ea1103d10abfd8b91818f94758ab06009db96
JJJYeong <bss05007@naver.com>
2024-12-31 15:29:20 +0900

    시스템코드 Repository 수정

--- PerformWorld/src/main/java/com/performworld/repository/systemcode/SystemCodeRepository.java
+++ PerformWorld/src/main/java/com/performworld/repository/systemcode/SystemCodeRepository.java
@@ -1,11 +1,21 @@
 package com.performworld.repository.systemcode;
 
 import com.performworld.domain.SystemCode;
+import com.performworld.dto.SysCodeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SystemCodeRepository extends JpaRepository<SystemCode,Long> {
+
     Optional<SystemCode> findByCodeName(String codeName);
+
     Optional<SystemCode> findByCode(String code);
+
+    @Query("SELECT new com.performworld.dto.SysCodeDTO" +
+            "(s.codeId, s.mainCode, s.subCode, s.code, s.codeCate, s.codeName) " +
+            "FROM SystemCode s WHERE s.mainCode = :mainCode ORDER BY s.subCode")
+    List<SysCodeDTO> getSysList(String mainCode);
 }
