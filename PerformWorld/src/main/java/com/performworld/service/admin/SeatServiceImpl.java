32ff3ce3674a06d89ba191d5e84e2f73ed8e8d2d
JJJYeong <bss05007@naver.com>
2025-01-02 11:27:23 +0900

    예매하기 좌석 조회 추가

--- PerformWorld/src/main/java/com/performworld/service/admin/SeatServiceImpl.java
+++ PerformWorld/src/main/java/com/performworld/service/admin/SeatServiceImpl.java
@@ -4,6 +4,7 @@ import com.performworld.domain.Seat;
 import com.performworld.dto.admin.SeatDTO;
 import com.performworld.repository.admin.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public class SeatServiceImpl implements SeatService {
     @Override
     public List<SeatDTO> getAllSeats() {
         // Retrieve all seats and map to SeatDTO
-        List<Seat> seats = seatRepository.findAll();
+        List<Seat> seats = seatRepository.findAll(Sort.by(Sort.Order.asc("seatId")));
         return seats.stream()
                 .map(seat -> new SeatDTO(seat.getSeatId(), seat.getSection(), seat.getPrice()))
                 .collect(Collectors.toList());
