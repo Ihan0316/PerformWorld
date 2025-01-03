e307b15e0d34429d5c1d74d78639fb30464ed466
이상현 <tkdgus3769@naver.com>
2025-01-03 15:03:36 +0900

    SeatServiceImpl getAllseat() 정렬 수정

--- PerformWorld/src/main/java/com/performworld/service/admin/SeatServiceImpl.java
+++ PerformWorld/src/main/java/com/performworld/service/admin/SeatServiceImpl.java
@@ -8,6 +8,7 @@ import com.performworld.repository.admin.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,7 +23,7 @@ public class SeatServiceImpl implements SeatService {
     @Override
     public List<SeatDTO> getAllSeats() {
         // Retrieve all seats and map to SeatDTO
-        List<Seat> seats = seatRepository.findAll();
+        List<Seat> seats = seatRepository.findAll(Sort.by(Sort.Order.asc("seatId")));
         return seats.stream()
                 .map(seat -> new SeatDTO(seat.getSeatId(), seat.getSection(), seat.getPrice()))
                 .collect(Collectors.toList());
