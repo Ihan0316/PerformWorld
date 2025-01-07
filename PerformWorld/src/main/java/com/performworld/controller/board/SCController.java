8d810d23414fec7d78e0f0df041b20dca42154f4
swdean07 <swdean07@gmail.com>
2025-01-07 21:48:41 +0900

    250107 SCController, SCService 내용 추가 실시

--- PerformWorld/src/main/java/com/performworld/controller/board/SCController.java
+++ PerformWorld/src/main/java/com/performworld/controller/board/SCController.java
@@ -1,14 +1,14 @@
 package com.performworld.controller.board;
 
-import com.performworld.domain.Notice;
+import com.performworld.dto.board.FAQDTO;
 import com.performworld.dto.board.NoticeDTO;
+import com.performworld.dto.board.QnADTO;
 import com.performworld.service.board.SCService;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,9 +23,24 @@ public class SCController {
     }
 
     @GetMapping("/list")
-    public String getNotice(Model model) {
-        List<NoticeDTO> noticePage = scService.getNotice();
+    public String getNotice(@ModelAttribute NoticeDTO noticeDTO, Model model) {
+        List<NoticeDTO> noticePage = scService.getNotice(noticeDTO);
         model.addAttribute("noticeList", noticePage);
-        return "board/sc";
+        return "board/sc"; // HTML 뷰 파일 경로
+    }
+
+    @GetMapping("/list")
+    public String getFaq(@ModelAttribute FAQDTO faqDTO, Model model) {
+        List<FAQDTO> faqPage = scService.getFaq(faqDTO);
+        model.addAttribute("faqList", faqPage);
+        return "board/sc"; // HTML 뷰 파일 경로
+    }
+
+
+    @GetMapping("/list")
+    public String getQnA(@ModelAttribute QnADTO qnaDTO, Model model) {
+        List<QnADTO> qnaPage = scService.getQna(qnaDTO);
+        model.addAttribute("qnaList", qnaPage);
+        return "board/sc"; // HTML 뷰 파일 경로
     }
 }
--- PerformWorld/src/test/java/com/performworld/service/SCRepositoryTests.java
+++ PerformWorld/src/test/java/com/performworld/service/SCRepositoryTests.java
@@ -1,6 +1,8 @@
 package com.performworld.service;
 
+import com.performworld.repository.board.FAQRepository;
 import com.performworld.repository.board.NoticeRepository;
+import com.performworld.repository.board.QnARepository;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.validator.constraints.Length;
@@ -13,9 +15,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class SCRepositoryTests {
     @Autowired
     private NoticeRepository noticeRepository;
+    private FAQRepository faqRepository;
+    private QnARepository qnARepository;
 
     @Test
     public void test() {
         log.info(noticeRepository.findAll());
     }
+
+//    @Test
+//    public void test2() {
+//        log.info(faqRepository.findAll());
+//    }
+//
+//    @Test
+//    public void test3() {
+//        log.info(qnARepository.findAll());
+//    }
 }
